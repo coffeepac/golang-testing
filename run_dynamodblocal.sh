615d8c97e64fe7fa4674266f849373ef555ad13a
Patrick Christopher <coffeepac@gmail.com>
2019-03-11 16:17:21 -0700

    log config, add run script

---
+++ run_dynamodblocal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# wrapper script to start the local dynamo db process.  
+# Any arguments to this script will be passed directly to the dynamodb process without being
+# parsed.
+
+set -xe 
+
+java -Djava.library.path=/var/dynamodb_wd/ -jar DynamoDBLocal.jar -sharedDb -dbPath /var/dynamodb_local $@
